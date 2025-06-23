ca35d7ebce7817253d594846442575a2a520eb0e
Lorin A <acheylorin@gmail.com>
2025-06-23 11:41:39 -0600

    Enable ground plane filtering and make the base frame match IsaacSim

--- octomap_server/src/octomap_server.cpp
+++ octomap_server/src/octomap_server.cpp
@@ -62,7 +62,7 @@ OctomapServer::OctomapServer(const rclcpp::NodeOptions & node_options)
   using std::placeholders::_2;
 
   world_frame_id_ = declare_parameter("frame_id", "map");
-  base_frame_id_ = declare_parameter("base_frame_id", "base_footprint");
+  base_frame_id_ = declare_parameter("base_frame_id", "base_link");
   use_height_map_ = declare_parameter("use_height_map", false);
   use_colored_map_ = declare_parameter("colored_map", false);
   color_factor_ = declare_parameter("color_factor", 0.8);
